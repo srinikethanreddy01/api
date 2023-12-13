5c9516a141ffa4c168004a9ce57090ddbd401d42
srinikethanreddy01 <153156304+srinikethanreddy01@users.noreply.github.com>
2023-12-13 11:56:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app=Flask("__name__")
 
 # Ensure the upload folder exists
 # os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-# CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 # CORS(app, supports_credentials=True) 
 
 @app.route('/', methods=['POST'])
